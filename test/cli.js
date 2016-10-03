c03f85b884cf51675219d2f700351d88d194fcb1
Evgeny Poberezkin <e.poberezkin@me.com>
2016-10-03 21:41:09 +0100

    test: use nyc for coverage

--- test/cli.js
+++ test/cli.js
@@ -2,13 +2,9 @@
 
 var exec = require('child_process').exec;
 var path = require('path');
-
-var RUN_CLI = process.env.CLI_TEST_COVERAGE == 'true'
-              ? 'istanbul cover --report none --print none --include-pid index.js -- '
-              : 'node index ';
 var CWD = path.join(__dirname, '..');
 
 
 module.exports = function cli(params, callback) {
-  exec(RUN_CLI + params, { cwd: CWD }, callback);
+  exec('node index ' + params, { cwd: CWD }, callback);
 };
