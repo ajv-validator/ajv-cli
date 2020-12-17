8c440a564fb712b3a2ed93d443e561117ab036b6
Joris Kraak <me@joriskraak.nl>
2020-12-17 17:36:59 +0100

    fix: set `$data` option in the presence of the `--data` switch
    Fixes #93.

--- src/commands/options.ts
+++ src/commands/options.ts
@@ -94,7 +94,10 @@ function parameter(str: string): string {
 export function getOptions(argv: ParsedArgs): Options & {code: CodeOptions} {
   const options: {[K in string]: any} = {code: {}}
   for (let opt in ajvOptions) {
-    if (opt === "data") opt = "$data"
+    if (opt === "data") {
+      opt = "$data"
+      argv.$data = argv.data
+    }
     const value = argv[toDashCase(opt)] ?? argv[opt]
     if (value === undefined) continue
     if (opt.startsWith(CODE)) {
